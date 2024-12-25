2952cd49f7164dd376adf52ac4d61d51cbf7cf08
totojack <tomjack.dev@gmail.com>
2024-12-25 00:50:36 -0300

    Nueva UI y Componentización

---
+++ src/components/AddFavCity.jsx
@@ -0,0 +1,7 @@
+export function AddFavCity() {
+	return (
+		<button className="pointer rounded-lg bg-white/5 text-white text-sm p-3 backdrop-blur-md hover:bg-white/10 transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-white/50 focus:ring-offset-2 focus:ring-offset-white/10 focus:ring-opacity-50 focus:ring-offset-opacity-50">
+			Add to favourites
+		</button>
+	)
+}
